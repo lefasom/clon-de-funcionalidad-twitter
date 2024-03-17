bc25d37b1c4820cb8810a22d4a0bfe799341ae68
lefasom <leosombra89@gmail.com>
2024-03-17 19:49:54 -0300

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react'
-import  Axios  from 'axios'
-import './App.css'
-
+import Router from "./routes/Router"
 function App() {
-  const [data, setData] = useState([])
-  const getTasks = async() => {
-    const resp = await Axios.get('http://localhost:3000/tasks')
-    setData(resp.data)
-  }
-  useEffect(()=>{
-    getTasks()
-  },[])
+
   return (
     <>
-   <h1>HOLA MUNDO</h1>
-   {data.map((val)=>{
-    return(
-    <div key={val.id}>
-      <h6>{val.title}</h6>
-      <p>{val.description}</p>
-    </div>
-    )
-  })}
+      <Router />
     </>
   )
 }
